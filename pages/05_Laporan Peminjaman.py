de0ffa5ffa771c33e2a29dcd8754054d8ad74040
harun al rasyid (edp) <5891@dnpi.co.id>
2025-01-06 20:54:11 +0700

    test

--- pages/05_Laporan Peminjaman.py
+++ pages/05_Laporan Peminjaman.py
@@ -11,4 +11,4 @@ st.title("List Peminjam")
 
 for index, item in enumerate(peminjam):
     # st.write(item[0])
-    st.write(f"{str(index + 1)} - {item[0]} - {item[1]} - tanggal Pinjam : {item[2]} - tanggal Pengembalian : {item[3]} ")
+    st.write(f"{str(index)} - {item[0]} - {item[1]} - tanggal Pinjam : {item[2]} - tanggal Pengembalian : {item[3]} ")
