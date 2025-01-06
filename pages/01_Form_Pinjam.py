d9935876d634ea3e9d222eee1d3ee15fa721fb0c
harun al rasyid (edp) <5891@dnpi.co.id>
2025-01-06 20:13:46 +0700

    test

--- pages/01_Form_Pinjam.py
+++ pages/01_Form_Pinjam.py
@@ -19,17 +19,17 @@ st.write("Silahkan Masukkan Data Diri Anda")
 if st.button("pinjem Buku" ):
     buku[0][1] = buku[0][1] - 1
 
-list_buku = []
-for item, index in enumerate(buku):
-    list_buku.append(str(item[3]))
+# list_buku = []
+# for item, index in enumerate(buku):
+#     list_buku.append(str(item[3]))
 
 #Input Form
 form1 = st.form(key="annotation1",clear_on_submit=True)
 with form1:
         cols = st.columns((1,1))
         nama = cols[0].text_input("Nama Lengkap :")
-        # judul = cols[1].selectbox('Pilih Judul Buku',('','As A Man Thinketh by James Allen','The Metamorphosis by Franz Kafka','1984 by George Orwell','Manusia Setengan Salmon by Raditya Dika','Ubur Ubur Lembur by Raditya Dika','Sang Pemimpi by Andrea Hirata','The Little Prince by Antonie De Saint-Exupery','The Laws Of Human Nature by Robert Greene','The Art Of Being Alone by Renuka Gavrani','Steal Like An Artist by Austin Kleon'))
-        judul = cols[1].selectbox('Pilih Judul Buku',list_buku)
+        judul = cols[1].selectbox('Pilih Judul Buku',('','0 - As A Man Thinketh by James Allen','1 - The Metamorphosis by Franz Kafka','2 - 1984 by George Orwell','3 - Manusia Setengan Salmon by Raditya Dika','4 - Ubur Ubur Lembur by Raditya Dika','5 - Sang Pemimpi by Andrea Hirata','6 - The Little Prince by Antonie De Saint-Exupery','7 - The Laws Of Human Nature by Robert Greene','8 - The Art Of Being Alone by Renuka Gavrani','9 - Steal Like An Artist by Austin Kleon'))
+        # judul = cols[1].selectbox('Pilih Judul Buku',list_buku)
         cols = st.columns(2)
         tglpinjam = cols[0].date_input("Tanggal Peminjaman :")
         tglkembali = cols[1].date_input("Tanggal Kembali :")
