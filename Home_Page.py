2d65da73128e12df4567e6887241c66e41fba192
harun al rasyid (edp) <5891@dnpi.co.id>
2025-01-06 18:56:11 +0700

    test

--- Home_Page.py
+++ Home_Page.py
@@ -42,12 +42,12 @@ cols1 = st.columns((1,1,1))
 cols2 = st.columns((1,1,1))
 cols3 = st.columns((1))
 
-for item in buku:
-    with cols[1]:
-        st.image(Image.open(item[0]), width=200)
-        st.write("Tersedia 5 Buku")
-        if st.button("Detail Buku 2"):
-            st.write(deskripsi_buku["2"])
+# for item in buku:
+#     with cols[1]:
+#         st.image(Image.open(item[0]), width=200)
+#         st.write("Tersedia 5 Buku")
+#         if st.button("Detail Buku 2"):
+#             st.write(deskripsi_buku["2"])
 
 # # Buku 1
 # with cols[0]:
