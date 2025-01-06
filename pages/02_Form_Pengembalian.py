188cd4da62b15d5f6f01f8327192b9ded3f33cf4
harun al rasyid (edp) <5891@dnpi.co.id>
2025-01-06 21:44:08 +0700

    test

--- pages/02_Form_Pengembalian.py
+++ pages/02_Form_Pengembalian.py
@@ -50,6 +50,7 @@ with form2:
             st.write(peminjam)
             # my_list = ['Harun al', 'A25325 - asfsafasf']
             peminjam = [sublist for sublist in peminjam if not any(id in item for item in sublist)]
+            st.write(peminjam)
             if(denda == 0):
                 st.success("Terimakasih sudah mengembalikan buku tepat pada waktunya!")
                 st.balloons()
