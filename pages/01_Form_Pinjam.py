b0db459bcd83599d477c33a2647703eb9ab10e71
chevy <chevy@dnp-g.com>
2025-01-08 17:26:51 +0700

    qr

--- pages/01_Form_Pinjam.py
+++ pages/01_Form_Pinjam.py
@@ -7,8 +7,8 @@ from Array_Buku import buku
 from Array_Peminjam import peminjam
 from Array_Peminjam import key_peminjam
 import datetime
-# import qrcode
-# from PIL import Image
+import qrcode
+from PIL import Image
 
 
 
@@ -66,24 +66,24 @@ with form1:
                 )
                 
                 key_peminjam.append( id + ' - ' + nama + " - " + judul)
-                # Generate QR code
-                # qr = qrcode.QRCode(
-                #     version=1,
-                #     error_correction=qrcode.constants.ERROR_CORRECT_L,
-                #     box_size=10,
-                #     border=4,
-                # )
-                # qr.add_data(id + ' - ' + nama + " - " + judul)
-                # qr.make(fit=True)
+                Generate QR code
+                qr = qrcode.QRCode(
+                    version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_L,
+                    box_size=10,
+                    border=4,
+                )
+                qr.add_data(id + ' - ' + nama + " - " + judul)
+                qr.make(fit=True)
 
-                # img = qr.make_image(fill='black', back_color='white')
-                # img_path = "qrcode-" + id + ".png"
-                # img.save(img_path)
+                img = qr.make_image(fill='black', back_color='white')
+                img_path = "qrcode-" + id + ".png"
+                img.save(img_path)
 
-                # # Display QR code in Streamlit
-                # st.image(img_path, caption="QR Code Peminjaman Buku")
+                # Display QR code in Streamlit
+                st.image(img_path, caption="QR Code Peminjaman Buku")
                 
-                #Menyimpan Data dan Membuat Struk
+                Menyimpan Data dan Membuat Struk
                 for i in range(1):
                     arynama.append(nama)
                     aryjudul.append(judul)
