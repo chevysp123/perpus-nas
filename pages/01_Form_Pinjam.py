9592453a8cd77bccd8e08bb98be99f48f74283ec
chevy <chevy@dnp-g.com>
2025-01-09 19:16:43 +0700

    qr

--- pages/01_Form_Pinjam.py
+++ pages/01_Form_Pinjam.py
@@ -32,7 +32,7 @@ st.write("Silahkan Masukkan Data Diri Anda")
 #Input Form
 form1 = st.form(key="annotation1",clear_on_submit=False)
 with form1:
-        cols = st.columns((1,1))
+        cols = st.columns((1))
         nama = cols[0].text_input("Nama Lengkap :")
         judul = cols[1].selectbox('Pilih Judul Buku',('','0 - As A Man Thinketh by James Allen','1 - The Metamorphosis by Franz Kafka','2 - 1984 by George Orwell','3 - Manusia Setengan Salmon by Raditya Dika','4 - Ubur Ubur Lembur by Raditya Dika','5 - Sang Pemimpi by Andrea Hirata','6 - The Little Prince by Antonie De Saint-Exupery','7 - The Laws Of Human Nature by Robert Greene','8 - The Art Of Being Alone by Renuka Gavrani','9 - Steal Like An Artist by Austin Kleon'))
         # judul = cols[1].selectbox('Pilih Judul Buku',list_buku)
