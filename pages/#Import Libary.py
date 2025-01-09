4182a8004d664d601c10a2928d8c81fbb8e29119
chevy <chevy@dnp-g.com>
2025-01-09 08:20:56 +0700

    qr

--- pages/#Import Libary.py
+++ pages/#Import Libary.py
@@ -1,24 +1,31 @@
-#Import Libary
-import time
-import sys
-import os
-import streamlit as st
-from Array_Buku import buku
-from Array_Peminjam import peminjam
-from Array_Peminjam import key_peminjam
-import datetime
 import qrcode
-from PIL import Image
+img = qrcode.make('Some data here')
+type(img)  # qrcode.image.pil.PilImage
+img.save("some_file.png")
 
 
-#Konfigurasi Halaman Streamlit
-st.set_page_config(page_title="Menu Peminjaman Buku", page_icon="📖", layout="centered")
 
-#Inisialisasi Variabel
-arynama,aryjudul,arytglpinjam,arytglkembali = [],[],[],[]
+# ##Import Libary
+# import time
+# import sys
+# import os
+# import streamlit as st
+# from Array_Buku import buku
+# from Array_Peminjam import peminjam
+# from Array_Peminjam import key_peminjam
+# import datetime
+# import qrcode
+# from PIL import Image
 
-#Elemen Halaman Streamlit
-st.empty()
-st.title("📖 Menu Peminjaman Buku")
-st.write("Silahkan Masukkan Data Diri Anda")
+
+# ##Konfigurasi Halaman Streamlit
+# st.set_page_config(page_title="Menu Peminjaman Buku", page_icon="📖", layout="centered")
+
+# #Inisialisasi Variabel
+# arynama,aryjudul,arytglpinjam,arytglkembali = [],[],[],[]
+
+# ##Elemen Halaman Streamlit
+# st.empty()
+# st.title("📖 Menu Peminjaman Buku")
+# st.write("Silahkan Masukkan Data Diri Anda")
 
