f74cb928902aefff782b1eb9897602965b2f6dd3
chevy <chevy@dnp-g.com>
2025-01-10 15:15:45 +0700

    pengambilan

--- pages/06_Form_pengambilan.py
+++ pages/06_Form_pengambilan.py
@@ -70,7 +70,7 @@ with form4:
                 if Array_Peminjam.peminjam[index][5] == 1:
                     st.error("QR Code sudah dipakai.")
                 else:
-                    Array_Peminjam.peminjam[index][5] == 1 
+                    Array_Peminjam.peminjam[index][5] = 1 
                     Array_Buku.buku[index_buku][1] -= 1
                     st.success(Array_Peminjam.peminjam[index][5])
                     st.success("Buku berhasil dipinjam.")
