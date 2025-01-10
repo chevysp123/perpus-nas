e5d596078a9e96599743dd4ec7fe62097d38362c
chevy <chevy@dnp-g.com>
2025-01-10 15:42:43 +0700

    pengambilan

--- pages/05_Laporan Peminjaman.py
+++ pages/05_Laporan Peminjaman.py
@@ -11,5 +11,5 @@ st.title("List Peminjam")
 
 for index, item in enumerate(peminjam):
     # st.write(item[0])
-    if item[0] == 1: 
+    if item[5] == 1: 
         st.write(f"{str(index + 1)} - {item[0]} - {item[1]} - {item[2]} - tanggal Pinjam : {item[3]} - tanggal Pengembalian : {item[4]} ")
