ab1df6afe066f173533b7d3c6d5e52e766407824
chevy <chevy@dnp-g.com>
2025-01-10 13:19:56 +0700

    pengambilan

--- pages/06_Form_pengambilan.py
+++ pages/06_Form_pengambilan.py
@@ -64,7 +64,7 @@ with form4:
             buku = str(nama.split(" - ")[2])
             index = next((i for i, sublist in enumerate(Array_Peminjam.peminjam) if id in sublist), None)
             index_buku = next((i for i, sublist in enumerate(Array_Buku.buku) if buku in sublist), None)
-            if index_buku is None or Array_Buku.buku[index_buku][1] == 0:
+            if Array_Buku.buku[index_buku][1] == 0:
                 st.error("Maaf, Buku yang Anda Pilih Sedang Tidak Tersedia");
             else:
                 if Array_Buku.buku[index_buku][5] == 1:
