b8af432bc936b195a058271665bba01121b42711
chevy <chevy@dnp-g.com>
2025-01-12 00:14:24 +0700

    pengembalian

--- pages/02_Form_Pengembalian.py
+++ pages/02_Form_Pengembalian.py
@@ -27,20 +27,21 @@ with form2:
         submitted = st.form_submit_button(label="Scan QR Code", on_click=None)
         nama = form2.text_input("scan Disini :")
         # judul = form2.selectbox('Pilih Judul Buku',('','As A Man Thinketh by James Allen','The Metamorphosis by Franz Kafka','1984 by George Orwell','Manusia Setengan Salmon by Raditya Dika','Ubur Ubur Lembur by Raditya Dika','Sang Pemimpi by Andrea Hirata','The Little Prince by Antonie De Saint-Exupery','The Laws Of Human Nature by Robert Greene','The Art Of Being Alone by Renuka Gavrani','Steal Like An Artist by Austin Kleon'))
-        tglkembali = form2.date_input("Tanggal Deadline Pengembalian :")
+        tglkembalidead = form2.date_input("Tanggal Deadline Pengembalian :")
         
         #Logika Perhitungan Denda
-        tglPinjam = Array_Peminjam.peminjam[4]
-        tglPinjam = datetime.datetime.strptime(tglPinjam, "%Y-%m-%d")
-        tglKembali = datetime.datetime.strptime(tglkembali, "%Y-%m-%d")
+        key = nama
+        if key in Array_Peminjam.key_peminjam:
+            index = Array_Peminjam.key_peminjam.index(key)
+            data_peminjam = Array_Peminjam.peminjam[index]
+            tglKembali = data_peminjam[4]
         
-        selisih = tglKembali - tglPinjam
-        totalHari = selisih.days
-        
-        denda = 0
-
-        if totalHari > 0:
-            denda = 5000 * totalHari
+            selisih = tglkembalidead - tglKembali
+            totalHari = selisih.days
+                
+            denda = 0
+            if totalHari > 0:
+                denda = 5000 * totalHari
 
         # tglskg = datetime.datetime.now()
         # tglkembali = str(tglkembali)
