815436476317d99f6830432425fa32d06d563b68
chevy <chevy@dnp-g.com>
2025-01-11 23:34:23 +0700

    m

--- pages/02_Form_Pengembalian.py
+++ pages/02_Form_Pengembalian.py
@@ -51,20 +51,55 @@ with form2:
 
         #Menampilkan Hasil
         if submitted: 
+            key = nama
+            if key in Array_Peminjam.key_peminjam:
+                index = Array_Peminjam.key_peminjam.index(key)
+                data_peminjam = Array_Peminjam.peminjam[index]
+                st.write("Data Peminjam:")
+                st.write(f"ID: {data_peminjam[0]}")
+                st.write(f"Nama: {data_peminjam[1]}")
+                st.write(f"Judul Buku: {data_peminjam[2]}")
+                st.write(f"Tanggal Pinjam: {data_peminjam[3]}")
+                st.write(f"Tanggal Kembali: {data_peminjam[4]}")
+                image_path = Array_Buku.buku[data_peminjam[6]][0]
+                image = Image.open(image_path)
+                st.image(image, caption=f"Tersedia: {Array_Buku.buku[data_peminjam[6]][1]} Buku", use_column_width=False) 
+            else:
+                st.error("Data tidak ditemukan.")
+            # id = str(nama.split(" - ")[0]) 
+            # buku = str(nama.split(" - ")[2]) 
+            # index = next((i for i, sublist in enumerate(Array_Peminjam.peminjam) if id in sublist), None)
+            # index_buku = next((i for i, sublist in enumerate(Array_Buku.buku) if buku in sublist), None)
+            # Array_Buku.buku[index_buku][1] += 1
+            # Array_Peminjam.peminjam.pop(index)
+            # Array_Peminjam.key_peminjam.pop(index)
+            
+            # if(denda == 0):
+            #     st.success("Terimakasih sudah mengembalikan buku tepat pada waktunya!")
+            #     st.balloons()
+            # else:
+            #     st.success("Anda terlambat mengembalikan buku sebanyak "+ str(totalHari) +" hari, maka harap membayar denda sebesar "+ str(denda) +" rupiah")
+
+
+form5 = st.form(key="annotation2", clear_on_submit=False)
+with form5:
+    submitted2 = st.form_submit_button(label="Serahkan Buku")
+    if submitted2:
+        if 'nama' not in locals() or not nama:
+            st.error("Silakan scan QR Code terlebih dahulu.")
+        else:
             id = str(nama.split(" - ")[0]) 
-            buku = str(nama.split(" - ")[2]) 
-            # peminjam.remove(0)
-            # Array_Peminjam.peminjam = [sublist for sublist in Array_Peminjam.peminjam if not any(id in item for item in sublist)]
+            buku = str(nama.split(" - ")[2])
             index = next((i for i, sublist in enumerate(Array_Peminjam.peminjam) if id in sublist), None)
             index_buku = next((i for i, sublist in enumerate(Array_Buku.buku) if buku in sublist), None)
-            Array_Buku.buku[index_buku][1] += 1
-            Array_Peminjam.peminjam.pop(index)
-            Array_Peminjam.key_peminjam.pop(index)
-            
-            if(denda == 0):
-                st.success("Terimakasih sudah mengembalikan buku tepat pada waktunya!")
-                st.balloons()
+            if Array_Buku.buku[index_buku][1] == 0:
+                st.error("Maaf, Buku yang Anda Pilih Sedang Tidak Tersedia");
             else:
-                st.success("Anda terlambat mengembalikan buku sebanyak "+ str(totalHari) +" hari, maka harap membayar denda sebesar "+ str(denda) +" rupiah")
+                if Array_Peminjam.peminjam[index][5] == 1:
+                    st.error("QR Code sudah dipakai.")
+                else:
+                    Array_Peminjam.peminjam[index][5] = 1 
+                    Array_Buku.buku[index_buku][1] -= 1
+                    st.success(Array_Peminjam.peminjam[index][5])
+                    st.success("Buku berhasil dipinjam.")
 
-                
