dddf1359e81d7845d9c0880ab586d66747b28bf4
chevy <chevy@dnp-g.com>
2025-01-11 11:19:00 +0700

    pengambilan

--- pages/06_Form_pengambilan.py
+++ pages/06_Form_pengambilan.py
@@ -51,8 +51,8 @@ with form3:
                 st.write(f"Tanggal Pinjam: {data_peminjam[3]}")
                 st.write(f"Tanggal Kembali: {data_peminjam[4]}")
                 image_path = Array_Buku.buku[data_peminjam[6]][0]
-                image = Image.open(image_path, width=200)
-                st.image(image, caption=st.write(f"Tersedia: {Array_Buku.buku[data_peminjam[6]][1]}"), use_column_width=True, width=20)
+                image = Image.open(image_path)
+                st.image(image, caption=st.write(f"Tersedia:" {Array_Buku.buku[data_peminjam[6]][1]}, + " Buku"), use_column_width=True, width=20)
             else:
                 st.error("Data tidak ditemukan.")
         
